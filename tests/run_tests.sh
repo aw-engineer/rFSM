4b8b504b5c4b2a2420ed562330796a0fdc103421
Andrew Wilson <87784967+aw-engineer@users.noreply.github.com>
2024-07-07 13:09:47 -0400

    updates run_tests.sh and github action

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
+# This unit test runner script supports several command line options
+# 
+# clean:
+# by default, test artifacts are removed at end of test
+# unset the default by passing --clean=
+#
+# coverage:
+# enables code coverage
+# requires
+#   - luarocks package manager
+#   - luacov project
+#
+# interp:
+# run tests with different lua interpreters
+# for example: --interp=lua5.3
+# for example: -i=lua5.2
+# for example: --interp="lua5.1 lua5.2 lua5.3"
+#
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" 
 OPTION_INTERP=lua
 OPTION_CLEAN=1
+OPTION_COVERAGE=
+
 TEST=`echo fsmtest_*`
 #TEST="test_simple.lua"
-
-export LUA_CPATH="/usr/lib/x86_64-linux-gnu/graphviz/lua/?.so"
 
 function show_help() {
   echo "$0 [--clean]=$OPTION_CLEAN [-i | --interp]=\"${OPTION_INTERP}\""
   exit 0
 }
 
-LONGOPTS=clean:,interp:,help
+LONGOPTS=clean:,coverage,interp:,help
 OPTIONS=i:,h
 PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@") || exit 2
 eval set -- "$PARSED"
 while true; do
     case "$1" in
+        --coverage)
+            OPTION_COVERAGE=1
+            shift 1
+            ;;
         --clean)
             OPTION_CLEAN="$2"
             shift 2
@@ -43,18 +65,41 @@ while true; do
     esac
 done
 
-cd ${SCRIPT_DIR}
+cd ${SCRIPT_DIR} # change to tests directory
 for interp in $OPTION_INTERP; do
     echo Using interpreter: $interp
-    
+
     set +e
     which $interp
     [[ $? -ne 0 ]] && continue
     set -e
+
+    PATTERN='^Lua ([0-9][.][0-9])'
+    [[ `$interp -v 2>&1` =~ $PATTERN ]] && LUA_VERSION=${BASH_REMATCH[1]} || exit
+
+    FLAGS=""
+    if [ $OPTION_COVERAGE ]; then
+        rm -vf luacov.stats* # zero the coverage statistics
+        FLAGS="-lluacov"
+        eval "$(luarocks --lua-version ${LUA_VERSION} path --bin)"
+    fi
+
+    # on ubuntu, libgv-lua provides libgv_lua.so
+    export LUA_CPATH="$LUA_CPATH;/usr/lib/x86_64-linux-gnu/graphviz/lua/?.so"
+
     for t in $TEST; do
         echo -e "\n\n*********************************** $interp $t ********************************************"
-        $interp $t
+        $interp $FLAGS $t
     done
+
+    if [ $OPTION_COVERAGE]; then
+        luacov
+        mv luacov.report.out luacov.report.out.${LUA_VERSION}
+    fi
+
 done
 
-[[ $OPTION_CLEAN ]] && rm -vf *.png
+if [ $OPTION_CLEAN ]; then
+    rm -vf *.png
+    rm -vf luacov.report.out* luacov.stats*
+fi
